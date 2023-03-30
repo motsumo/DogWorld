75ab68ee68da37a332ef0ca97c611f6b47e324c1
Katsiaryna Kamiak <kate_zhukovskaya@mail.ru>
2023-03-30 18:33:21 +0300

    Initial setup
    Prapared initial setup. Created small initial core components.

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutComponent } from './core/layout/layout.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PawIconComponent } from './core/Icons/paw-icon/paw-icon.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PawIconComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/Icons/paw-icon/paw-icon.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PawIconComponent } from './paw-icon.component';
+
+describe('PawIconComponent', () => {
+  let component: PawIconComponent;
+  let fixture: ComponentFixture<PawIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PawIconComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PawIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
